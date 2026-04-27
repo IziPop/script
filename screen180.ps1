b6ae5c092221080b3d90623ab6bc2131be265776
Martin <94807972+IziPop@users.noreply.github.com>
2026-04-27 14:12:48 +0200

    Add PowerShell script to change display orientation

---
+++ screen180.ps1
@@ -0,0 +1,38 @@
+Add-Type @"
+using System;
+using System.Runtime.InteropServices;
+
+public class Display {
+    [DllImport("user32.dll")]
+    public static extern bool EnumDisplaySettings(string deviceName, int modeNum, ref DEVMODE devMode);
+    
+    [DllImport("user32.dll")]
+    public static extern int ChangeDisplaySettings(ref DEVMODE devMode, int flags);
+
+    [StructLayout(LayoutKind.Sequential)]
+    public struct DEVMODE {
+        [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 32)]
+        public string dmDeviceName;
+        public short dmSpecVersion, dmDriverVersion, dmSize, dmDriverExtra;
+        public int dmFields;
+        public int dmPositionX, dmPositionY, dmDisplayOrientation, dmDisplayFixedOutput;
+        public short dmColor, dmDuplex, dmYResolution, dmTTOption, dmCollate;
+        [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 32)]
+        public string dmFormName;
+        public short dmLogPixels;
+        public int dmBitsPerPel, dmPelsWidth, dmPelsHeight, dmDisplayFlags, dmDisplayFrequency;
+        public int dmICMMethod, dmICMIntent, dmMediaType, dmDitherType, dmReserved1, dmReserved2;
+        public int dmPanningWidth, dmPanningHeight;
+    }
+}
+"@
+
+$dm = New-Object Display+DEVMODE
+$dm.dmSize = [System.Runtime.InteropServices.Marshal]::SizeOf($dm)
+[Display]::EnumDisplaySettings($null, -1, [ref]$dm)
+
+# 0 = Normal, 1 = 90°, 2 = 180°, 3 = 270°
+$dm.dmDisplayOrientation = 2
+$dm.dmFields = 0x00000080
+
+[Display]::ChangeDisplaySettings([ref]$dm, 0)
